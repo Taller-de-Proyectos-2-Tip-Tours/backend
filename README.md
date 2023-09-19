268c3531938ff1ad51a9262f885f8ca164138543
Diego Balestieri <diego.balestieri1998@gmail.com>
2023-09-19 16:52:10 -0300

    added all fields to trip

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # Como correr el backend
 
 ```
-sudo docker build -t backend .
-
-sudo docker run -p 5000:5000 backend 
+sudo docker build -t backend . && sudo docker run -p 5000:5000 backend 
 ```
 
 # Como correr los tests con code coverage
 
 ```
-pytest --cov-report term --cov=./ tests/ --cov-fail-under=70
+python3 -m pytest --cov-report term --cov=./ tests/ --cov-fail-under=70
 ```
 Si tienen problemas de dependencias ejecutar:
 
--- api/tours.py
+++ api/tours.py
@@ -4,10 +4,14 @@ from db.cities import CitiesCollection
 from flask import request, jsonify
 import json
 from marshmallow import Schema, fields, ValidationError, validates_schema
+import time
+from datetime import datetime
 
 tours = Blueprint('tours',__name__)
 tours_collection = ToursCollection()
 cities_collection = CitiesCollection()
+
+lenguages = ["Español", "Inglés", "Portugués", "Alemán", "Francés", "Italiano"]
 
 class ToursSchema(Schema):
     name = fields.String(required=True)
@@ -16,16 +20,65 @@ class ToursSchema(Schema):
     minParticipants = fields.Integer(required=True)
     maxParticipants = fields.Integer(required=True)
     city = fields.String(required=True)
-   
+    lenguage = fields.String(required=True)
+    meetingPoint = fields.String(required=True)
+    considerations = fields.String(required=True)
+    dates = fields.List(fields.String(), required=True)
+    mainImage = fields.String(required=True)
+    otherImages = fields.List(fields.String(), required=True)
+
+    @validates_schema
+    def validate_name(self, data, **kwargs):
+      if len(data['name']) > 50:
+        raise ValidationError("El nombre del paseo no puede contener más de 50 caracteres")
+    
+    @validates_schema
+    def validate_description(self, data, **kwargs):
+      if len(data['description']) > 200:
+        raise ValidationError("La descripción del paseo no puede contener más de 200 caracteres")
+      
+    @validates_schema
+    def validate_meeting_point(self, data, **kwargs):
+      if len(data['meetingPoint']) > 200:
+        raise ValidationError("Los puntos a tener en cuenta del paseo no puede contener más de 200 caracteres")
+
     @validates_schema
     def validate_participants(self, data, **kwargs):
       if data['minParticipants'] > data['maxParticipants']:
-        raise ValidationError("The minimum of participants should be less than the maximum")
+        raise ValidationError("El minimo de participantes debe ser menor al máximo")
       
     @validates_schema
     def validate_city(self, data, **kwargs):
       if cities_collection.get_city(data['city']) == None:
-        raise ValidationError("The city selected is not available")
+        raise ValidationError("La ciudad seleccionada no está disponible")
+      
+    @validates_schema
+    def validate_duration(self, data, **kwargs):
+        try:
+            time.strptime(data["duration"], '%H:%M')
+        except ValueError:
+            raise ValidationError("La duración debe respetar el formato HH:MM")
+        
+    @validates_schema
+    def validate_lenguage(self, data, **kwargs):
+       if not data["lenguage"] in lenguages:
+          raise ValidationError("El idioma seleccionado no está disponible")
+       
+    @validates_schema
+    def validate_dates(self, data, **kwargs):
+       for date in data["dates"]:
+            try:
+                formated_date = datetime.fromisoformat(date)
+                if formated_date.hour == 0 and formated_date.minute == 0:
+                   raise ValueError
+            except:
+                raise ValidationError("La fecha seleccionada no cumple con el formato ISO")
+            
+    @validates_schema
+    def validate_other_images(self, data, **kwargs):
+       length = len(data["otherImages"])
+       if length < 2 or length > 4:
+          raise ValidationError("El paseo debe contener entre 2 y 4 imagenes extras")
 
 @tours.route("/tours", methods=['GET'])
 def get_tours():
