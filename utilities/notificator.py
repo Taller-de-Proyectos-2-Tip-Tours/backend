05a6adeb6ff7fe310a47020de3e9b0483a5fae34
Diego Balestieri <diego.balestieri1998@gmail.com>
2023-10-21 20:23:05 -0300

    quick fix

--- utilities/notificator.py
+++ utilities/notificator.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 class Notificator:
-  _serverToken = "AAAASVPSQX8:APA91bGxjsKTkHWF5FEEg7ZCvCORC3F_oYFBw4kvOJ2EInaYKYnuhXtqFVyEk9f1_YJvZgo-QJ_Bwf1i5gK_d88bNzxGJZK1ED-g5vaomCzfPjm0fblPyE2W0l6VI1K45OOH1crYlA1A"
-  _deviceToken = "caiM-UoJTcOAohL64R3qMu:APA91bFDyX8UDCQXts12xWPTDYLCRT5CEpTrk18P5zHpELORahg34IYhj_e3J27lEoNozUAMJyGn_0BT_sGN0EuqOZ0gQ3ZC2s6kGdG0KWR4f6KxHHpt-xEjVXlo0Jm67UggNO1j4aKX"
+  _serverToken = "hola"
+  _deviceToken = "chau"
 
   def send_notification(self, title, message):
     headers = {
