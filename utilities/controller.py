522f3b586af6e83c3839c6388e661f811c36cee7
Diego Balestieri <diego.balestieri1998@gmail.com>
2023-10-29 16:44:35 -0300

    fix end reserve

--- utilities/controller.py
+++ utilities/controller.py
@@ -3,6 +3,7 @@ from db.tours_db import ToursCollection
 from db.reserves_db import ReservesCollection
 from utilities.notificator import Notificator
 import json
+import pytz
 
 class Controller:
   _tours_collection = ToursCollection()
@@ -10,8 +11,8 @@ class Controller:
   _notificator = Notificator()
   
   def end_tours(self):
-    print("Executing end tours process")
-    today = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    argentina_timezone = pytz.timezone('America/Argentina/Buenos_Aires')
+    today = datetime.now(argentina_timezone).strftime("%Y-%m-%dT%H:%M:%S")
     tours = json.loads(self._tours_collection.get_old_dates(today))
     for tour in tours:
       new_dates = []
@@ -23,10 +24,10 @@ class Controller:
       self._tours_collection.update_tour_dates(new_dates, tour["_id"]["$oid"])
       reserves = json.loads(self._reserves_collection.get_reserves_for_tour(tour["_id"]["$oid"]))
       for reserve in reserves:
-        self._reserves_collection.change_reserve_state(reserve["_id"]["$oid"], "finalizado")
+        if reserve["date"] < today:
+          self._reserves_collection.change_reserve_state(reserve["_id"]["$oid"], "finalizado")
 
   def reserve_reminder(self):
-    print("Executing reserve reminder")
     current_time = datetime.now()
     future_time = current_time + timedelta(hours=24)
     future_time_str = future_time.strftime("%Y-%m-%dT%H:%M:%S")
@@ -40,4 +41,3 @@ class Controller:
       }
       self._notificator.notify_reserve_reminder(reserve["traveler"]["email"], tour_data)
       self._reserves_collection.mark_notified(reserve['_id']['$oid'])
-    print(reserves)
