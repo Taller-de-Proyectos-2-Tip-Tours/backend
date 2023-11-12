26e34cf6682fa8c8140c3244661a1ae8ae7ae5ea
Diego Balestieri <diego.balestieri1998@gmail.com>
2023-11-12 17:08:00 -0300

    add image to notification

--- utilities/notificator.py
+++ utilities/notificator.py
@@ -9,17 +9,17 @@ class Notificator:
   _reserves = ReservesCollection()
   _users = UsersCollection()
 
-  def _send_notification(self, title, message, deviceToken, data):
+  def _send_notification(self, title, message, deviceToken, data, image = os.getenv("neutralImage")):
+    print(image)
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + self._serverToken,
       }
-    body = {'notification': { 'title': title, 'body': message }, 
+    body = {'notification': { 'title': title, 'body': message, 'image': image }, 
             'data': data,
               'to': deviceToken,
               'priority': 'high'
             }
-    print(body)
     response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headers, data=json.dumps(body))
     print(response.json())
     if response.json()['failure']:
@@ -34,7 +34,9 @@ class Notificator:
         self._send_notification("Paseo cancelado", 
                                 "Disculpe, la fecha de su paseo ha sido cancelado. Lamentamos cualquier inconveniente causado.", 
                                 token,
-                                tourData)
+                                tourData,
+                                os.getenv("sadImage"))
+        updatedDeviceTokens.append(token)
       except Exception as e:
         print(e)
     self._users.update_device_tokens_for_user(tokens["_id"]["$oid"], updatedDeviceTokens)
@@ -42,12 +44,14 @@ class Notificator:
   def notify_modified_tour(self, userEmail, tourData):
     tokens = self._users.get_device_token_by_email(userEmail)
     updatedDeviceTokens = []
+    print(userEmail)
     for token in tokens["devicesTokens"]:
       try:
         self._send_notification("Paseo modificado", 
                                 "Se ha realizado un cambio en los detalles de su paseo. Le solicitamos revisar la información actualizada.", 
                                 token,
                                 tourData)
+        updatedDeviceTokens.append(token)
       except Exception as e:
         print(e)
     self._users.update_device_tokens_for_user(tokens["_id"]["$oid"], updatedDeviceTokens)
@@ -60,7 +64,8 @@ class Notificator:
         self._send_notification("Recordatorio de reserva", 
                                 "Recuerde que tiene una reserva para un paseo a menos de 24 horas. Le solicitamos revisar los detalles y prepararse.", 
                                 token,
-                                tourData)
+                                tourData,
+                                os.getenv("happyImage"))
         updatedDeviceTokens.append(token)
       except Exception as e:
         print(e)
